6eea449c882ebe4bdb1b065d939fd83daf898dcc
星阳628 <151153234+LittleGui11@users.noreply.github.com>
2026-09-08 15:10:10 +0800

    Delete Stop.ps1

--- Stop.ps1
+++
@@ -1,7 +0,0 @@
-Get-CimInstance Win32_Process -Filter "Name='powershell.exe'" |
-  Where-Object { $_.CommandLine -match 'TaskbarStyleTool\\Run\.ps1' } |
-  ForEach-Object {
-    Write-Host ("Stopping PID " + $_.ProcessId)
-    Stop-Process -Id $_.ProcessId -Force -ErrorAction SilentlyContinue
-  }
-Write-Host "Done."
